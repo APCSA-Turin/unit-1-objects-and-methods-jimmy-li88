5b6dfee98fc41aeb49f409fd6884b58491381e3e
Jimmy Li <jli8239@bths.edu>
2025-10-23 18:01:32 +0000

    save

---
+++ U1T11Folder/CustomStringMethodsRunner.java
@@ -0,0 +1,25 @@
+package U1T11Folder;
+
+public class CustomStringMethodsRunner {
+
+    public static void main(String[] args) {
+        CustomStringMethods methods = new CustomStringMethods();
+        String str = methods.yellOrWhisper("Hello James!");
+        System.out.println(str);
+        str = methods.yellOrWhisper("hello James!");
+        System.out.println(str);
+        str = methods.yellOrWhisper("aBCDEFGHIJK");
+        System.out.println(str);
+        str = methods.yellOrWhisper("Abcdefghijk");
+        System.out.println(str);
+        str = methods.yellOrWhisper("B");
+        System.out.println(str);
+        str = methods.yellOrWhisper("b");
+        System.out.println(str);
+        str = methods.yellOrWhisper("IT'S SUNNY!");
+        System.out.println(str);
+        str = methods.yellOrWhisper("it's rainy");
+        System.out.println(str);
+
+    }
+}
--- U1T11Folder/Lab2.java
+++ U1T11Folder/Lab2.java
@@ -2,15 +2,40 @@ package U1T11Folder;
 import java.util.Scanner;
 public class Lab2 {
     public static void main(String[] args) {
-        Scanner equalWords = new Scanner(System.in);
-        System.out.print("Enter the first string: ");
-        String str1 = equalWords.nextLine();
-        System.out.print("Enter the second string: ");
-        String str2 = equalWords.nextLine();
-        if (str1.equals(str2)) {
-            System.out.println("The strings are equal.");
-        } else {
-            System.out.println("The strings are not equal.");
-    }
+        int score = 0;
+        int numWords = 0;
+        Scanner guessWord = new Scanner(System.in);
+        System.out.println("enter a word: ");
+        String word1 = guessWord.nextLine();
+
+        while (score < 50) {
+            System.out.println("enter a word: ");
+            String word2 = guessWord.nextLine();
+            numWords++;
+            if (word1.compareTo(word2) < 0) {
+                score += 2;
+            }
+            if (word1.compareTo(word2) > 0) {
+                score -= 5;
+            }
+            if (word1.equals(word2)){ 
+                score -= 10;
+            }
+            String lastLetters = word1.substring(word1.length() - 2);
+            String firstLetters = word2.substring(0, 2);
+            if (lastLetters.equals(firstLetters)) {
+                score += 5;
+            }
+            String firstLetter = word1.substring(0, 1);
+            if (word2.indexOf(firstLetter) != -1) {
+                score += 3;
+            }
+            word1 = word2;
+        }
+    System.out.println("Final score: " + score);
+    System.out.println("tries " + numWords);
 }
 }
+
+
+
